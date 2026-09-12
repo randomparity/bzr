ab4fb4d27cfa8f0e9a225338bfbc47cb29aae25e
David Christensen <randomparity@gmail.com>
2026-09-12 11:21:40 -0700

    fix(functional): follow rhbz setup ordering

--- tests/functional/versions/rhbz/entrypoint.sh
+++ tests/functional/versions/rhbz/entrypoint.sh
@@ -18,7 +18,7 @@ mkdir -p /var/www/html/bugzilla/data
 touch /var/www/html/bugzilla/data/bz.log /var/www/html/bugzilla/extensions/RedHat/info.log
 mkdir /tmp/rhbz-extensions
 mv extensions/ExternalBugs extensions/RedHat extensions/SubComponents /tmp/rhbz-extensions/
-perl checksetup.pl answers.txt
+perl checksetup.pl
 perl checksetup.pl answers.txt
 mv /tmp/rhbz-extensions/ExternalBugs /tmp/rhbz-extensions/RedHat /tmp/rhbz-extensions/SubComponents extensions/
 perl checksetup.pl answers.txt
