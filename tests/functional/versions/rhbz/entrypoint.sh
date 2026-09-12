1fa33699cbc0257d6ac45935a01f35c2158f0046
David Christensen <randomparity@gmail.com>
2026-09-12 11:23:45 -0700

    fix(functional): generate rhbz local configuration

--- tests/functional/versions/rhbz/entrypoint.sh
+++ tests/functional/versions/rhbz/entrypoint.sh
@@ -12,13 +12,16 @@ done
 mysqladmin ping --silent
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS bugs; GRANT ALL ON bugs.* TO 'bugs'@'localhost' IDENTIFIED BY 'bugzilla'; FLUSH PRIVILEGES;"
 cd /var/www/html/bugzilla
+rm localconfig
+perl checksetup.pl
+printf '%s\n' "\$db_driver = \"mysql\";" "\$db_host = \"localhost\";" "\$db_name = \"bugs\";" \
+    "\$db_user = \"bugs\";" "\$db_pass = \"bugzilla\";" "\$webservergroup = \"apache\";" >> localconfig
 printf '%s\n' "\$answer{'ADMIN_EMAIL'} = 'admin@test.bzr';" "\$answer{'ADMIN_PASSWORD'} = 'FuncTest1!';" "\$answer{'ADMIN_REALNAME'} = 'Admin User';" "\$answer{'ext_logins'} = '';" "\$answer{'NO_PAUSE'} = 1;" > answers.txt
 # RedHat reads core tables while its extension schema loads. Build the core schema first.
 mkdir -p /var/www/html/bugzilla/data
 touch /var/www/html/bugzilla/data/bz.log /var/www/html/bugzilla/extensions/RedHat/info.log
 mkdir /tmp/rhbz-extensions
 mv extensions/ExternalBugs extensions/RedHat extensions/SubComponents /tmp/rhbz-extensions/
-perl checksetup.pl
 perl checksetup.pl answers.txt
 mv /tmp/rhbz-extensions/ExternalBugs /tmp/rhbz-extensions/RedHat /tmp/rhbz-extensions/SubComponents extensions/
 perl checksetup.pl answers.txt
