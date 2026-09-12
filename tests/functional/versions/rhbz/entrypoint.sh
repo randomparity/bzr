d1ea21c5a16fe136c5ab85a439ed32e219bcc39d
David Christensen <randomparity@gmail.com>
2026-09-12 11:20:05 -0700

    fix(functional): prepare rhbz logs before setup

--- tests/functional/versions/rhbz/entrypoint.sh
+++ tests/functional/versions/rhbz/entrypoint.sh
@@ -14,13 +14,13 @@ mysql -u root -e "CREATE DATABASE IF NOT EXISTS bugs; GRANT ALL ON bugs.* TO 'bu
 cd /var/www/html/bugzilla
 printf '%s\n' "\$answer{'ADMIN_EMAIL'} = 'admin@test.bzr';" "\$answer{'ADMIN_PASSWORD'} = 'FuncTest1!';" "\$answer{'ADMIN_REALNAME'} = 'Admin User';" "\$answer{'ext_logins'} = '';" "\$answer{'NO_PAUSE'} = 1;" > answers.txt
 # RedHat reads core tables while its extension schema loads. Build the core schema first.
+mkdir -p /var/www/html/bugzilla/data
+touch /var/www/html/bugzilla/data/bz.log /var/www/html/bugzilla/extensions/RedHat/info.log
 mkdir /tmp/rhbz-extensions
 mv extensions/ExternalBugs extensions/RedHat extensions/SubComponents /tmp/rhbz-extensions/
 perl checksetup.pl answers.txt
 perl checksetup.pl answers.txt
 mv /tmp/rhbz-extensions/ExternalBugs /tmp/rhbz-extensions/RedHat /tmp/rhbz-extensions/SubComponents extensions/
-mkdir -p /var/www/html/bugzilla/data /var/www/html/bugzilla/extensions/RedHat
-touch /var/www/html/bugzilla/data/bz.log /var/www/html/bugzilla/extensions/RedHat/info.log
 perl checksetup.pl answers.txt
 perl checksetup.pl answers.txt
 exec httpd -D FOREGROUND
