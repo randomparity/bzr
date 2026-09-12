f959e2f4aeebdac91942fae3e8af9269846eef05
David Christensen <randomparity@gmail.com>
2026-09-12 11:25:06 -0700

    fix(functional): initialize rhbz schema explicitly

--- tests/functional/versions/rhbz/entrypoint.sh
+++ tests/functional/versions/rhbz/entrypoint.sh
@@ -22,8 +22,8 @@ mkdir -p /var/www/html/bugzilla/data
 touch /var/www/html/bugzilla/data/bz.log /var/www/html/bugzilla/extensions/RedHat/info.log
 mkdir /tmp/rhbz-extensions
 mv extensions/ExternalBugs extensions/RedHat extensions/SubComponents /tmp/rhbz-extensions/
-perl checksetup.pl answers.txt
+perl checksetup.pl --update-db answers.txt
 mv /tmp/rhbz-extensions/ExternalBugs /tmp/rhbz-extensions/RedHat /tmp/rhbz-extensions/SubComponents extensions/
-perl checksetup.pl answers.txt
+perl checksetup.pl --update-db answers.txt
 perl checksetup.pl answers.txt
 exec httpd -D FOREGROUND
