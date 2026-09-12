cc4ab9287edb67cc4402eb01fde233e8f75d5205
David Christensen <randomparity@gmail.com>
2026-09-12 11:34:27 -0700

    fix(functional): align rhbz group foreign key type

--- tests/functional/versions/rhbz/entrypoint.sh
+++ tests/functional/versions/rhbz/entrypoint.sh
@@ -22,7 +22,7 @@ mkdir -p /var/www/html/bugzilla/data
 touch /var/www/html/bugzilla/data/bz.log /var/www/html/bugzilla/extensions/RedHat/info.log
 if ! perl checksetup.pl --update-db answers.txt; then
     echo '==> RHBZ schema migration requires a second pass'
-    mysql -u root bugs -e 'ALTER TABLE report_groups MODIFY report_id MEDIUMINT UNSIGNED NOT NULL;'
+    mysql -u root bugs -e 'ALTER TABLE report_groups MODIFY report_id MEDIUMINT UNSIGNED NOT NULL, MODIFY group_id MEDIUMINT UNSIGNED NOT NULL;'
 fi
 perl checksetup.pl --update-db answers.txt
 perl checksetup.pl --update-db answers.txt
