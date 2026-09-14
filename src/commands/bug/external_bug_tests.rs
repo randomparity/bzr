554b9d9e24d7edc57d82e7828bc75a844389524d
David Christensen <randomparity@gmail.com>
2026-09-13 22:21:11 -0700

    test(bug): cover external tracker mutations

--- src/commands/bug/external_bug_tests.rs
+++ src/commands/bug/external_bug_tests.rs
@@ -1,6 +1,9 @@
 #![expect(clippy::unwrap_used)]
 
-use crate::cli::{ExternalBugAction, ExternalBugArgs, RemoveExternalBugArgs};
+use crate::cli::{
+    AddExternalBugArgs, ExternalBugAction, ExternalBugArgs, RemoveExternalBugArgs,
+    UpdateExternalBugArgs,
+};
 use crate::types::OutputFormat;
 
 #[tokio::test]
@@ -89,4 +92,84 @@ async fn external_bug_dry_run_does_not_connect() {
     .unwrap();
     let output: serde_json::Value = crate::test_helpers::json_envelope_data(io.out_str());
     assert_eq!(output["action"], "dry-run");
+}
+
+#[tokio::test]
+async fn external_bug_mutations_call_the_advertised_xmlrpc_extension() {
+    use wiremock::matchers::{body_string_contains, method, path};
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    let mock = MockServer::start().await;
+    let tmp = tempfile::TempDir::new().unwrap();
+    let config_path = crate::test_helpers::write_config_to(
+        &tmp,
+        &format!("default_server = \"rhbz\"\n\n[servers.rhbz]\nurl = \"{}\"\napi_key = \"test-key\"\nemail = \"test@example.com\"\n", mock.uri()),
+    );
+    Mock::given(method("GET"))
+        .and(path("/rest/version"))
+        .respond_with(
+            ResponseTemplate::new(200).set_body_json(serde_json::json!({"version":"5.1.2"})),
+        )
+        .mount(&mock)
+        .await;
+    Mock::given(method("GET"))
+        .and(path("/rest/valid_login"))
+        .respond_with(ResponseTemplate::new(200).set_body_json(serde_json::json!({"result":true})))
+        .mount(&mock)
+        .await;
+    Mock::given(method("GET"))
+        .and(path("/rest/extensions"))
+        .respond_with(
+            ResponseTemplate::new(200)
+                .set_body_json(serde_json::json!({"extensions":{"ExternalBugs":{}}})),
+        )
+        .mount(&mock)
+        .await;
+    for method_name in [
+        "ExternalBugs.add_external_bug",
+        "ExternalBugs.update_external_bug",
+        "ExternalBugs.remove_external_bug",
+    ] {
+        Mock::given(method("POST")).and(path("/xmlrpc.cgi")).and(body_string_contains(format!("<methodName>{method_name}</methodName>"))).respond_with(ResponseTemplate::new(200).set_body_string("<?xml version=\"1.0\"?><methodResponse><params><param><value><struct/></value></param></params></methodResponse>")).mount(&mock).await;
+    }
+    let actions = [
+        ExternalBugAction::Add(AddExternalBugArgs {
+            id: 42,
+            tracker: 7,
+            external_id: "EXT-1".into(),
+            status: "NEW".into(),
+            description: "created".into(),
+        }),
+        ExternalBugAction::Update(UpdateExternalBugArgs {
+            id: 42,
+            tracker: 7,
+            external_id: "EXT-1".into(),
+            status: "ASSIGNED".into(),
+            description: "updated".into(),
+        }),
+        ExternalBugAction::Remove(RemoveExternalBugArgs {
+            id: 42,
+            tracker: 7,
+            external_id: "EXT-1".into(),
+        }),
+    ];
+    for action in actions {
+        let mut io = crate::test_helpers::CapturedIo::new();
+        crate::commands::bug::execute(
+            &crate::cli::BugAction::ExternalBug(ExternalBugArgs { action }),
+            &crate::commands::runtime::invocation::CommandContext::new(
+                None,
+                OutputFormat::Json,
+                None,
+            )
+            .with_config_path_override(Some(config_path.clone())),
+            &mut io.writers(),
+        )
+        .await
+        .unwrap();
+        assert_eq!(
+            crate::test_helpers::json_envelope_data(io.out_str())["resource"],
+            "external_bug"
+        );
+    }
 }
